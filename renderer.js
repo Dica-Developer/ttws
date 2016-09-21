71dcd97686c377ddb847b0106dc4cbf8bb621b03
Martin Schaaf <mschaaf@datameer.com>
2016-09-21 23:34:50 +0200

    * include font awesome for icons especially clock icon * include furtive and style front page * exec ttwatch app * add watch version tag that renders model and firmware

--- renderer.js
+++ renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const exec = require('child_process').execFile;
+const child = exec('/usr/local/bin/ttwatch', ['-v'],
+  (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  }
+);
+
---
+++ watchversion.tag
@@ -0,0 +1,19 @@
+<watchversion>
+  <p>Firmware: { firmware }</p>
+  <p>Model: { model }</p>
+</div>
+  <script>
+    this.firmware = 'x-x-x'
+    this.model = 'x-x-x'
+
+    updateVersion() {
+      this.update({
+        firmware: 'y-y-y', 
+        model: 'y-y-y' 
+      })
+    }
+
+  </script>
+
+</watchversion>
+
