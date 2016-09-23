d17130b6d4aeb0e4510b2fe2340e8dca5e3da9f9
Martin Schaaf <mschaaf@datameer.com>
2016-09-23 17:18:01 +0200

    * add a dialog tag for user messages * add progress show by spinning the clock icon * move clock icon to icon tag * add update firmware tag and functionality * pull every 5 seconds the version to check if the watch is still connected * moved tag files to tags folder

--- renderer.js
+++ renderer.js
@@ -2,22 +2,59 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+// todo: sending the events not directly on there tags instead use a neutral way
+
 const exec = require('child_process').execFile;
 
 var watchversion = {};
+var updatebutton = {};
+var icon = {};
+var succesdialog = {};
 riot.compile(function() {
   watchversion = riot.mount('watchversion')[0];
-});
-exec('/usr/local/bin/ttwatch', ['-v'],
-  (error, stdout, stderr) => {
-    if (error !== null) {
-      console.error(error);
-      watchversion.trigger('watch.version.error', error.message);
-    } else {
-      watchversion.trigger('watch.version.succes', stdout);
-    }
+  updatebutton = riot.mount('updatebutton')[0];
+  icon = riot.mount('icon')[0];
+  succesdialog = riot.mount('succesdialog')[0];
+
+  function getVersion() {
+    icon.trigger('watch.progress.start');
+    exec('/usr/local/bin/ttwatch', ['-v'],
+      (error, stdout, stderr) => {
+        if (error !== null) {
+          console.error(error);
+          watchversion.trigger('watch.version.error', error.message);
+        } else {
+          watchversion.trigger('watch.version.succes', stdout);
+        }
+        icon.trigger('watch.progress.stop');
+      }
+    );
   }
-);
+
+  updatebutton.on('watch.update.firmware', function() {
+    icon.trigger('watch.progress.start');
+    exec('/usr/local/bin/ttwatch', ['--update-fw'],
+      (error, stdout, stderr) => {
+        if (error !== null) {
+          console.error(error);
+          watchversion.trigger('watch.update.error', error.message);
+        } else {
+          let message = stdout;
+          if (message === '') {
+            // Thats a fallback because in case of up to date the output goes to stderr but the app is returning with succes
+            message = stderr;
+          }
+          succesdialog.trigger('watch.succesdialog.message', message);
+        }
+        icon.trigger('watch.progress.stop');
+      }
+    );
+  });
+
+  getVersion();
+  setInterval(getVersion, 5000);
+});
+
 
 // ttwatch --get-activities --activity-store=./ goes under ./<watch name>/<date>/<file.ttbin>
 // ttbincnv -g mascha/2016-09-22/Cycling_16-44-30.ttbin to gpx file
