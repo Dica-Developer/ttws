57de4c0110c04b62099d1eef314d4113dc6670a3
Martin Schaaf <mschaaf@datameer.com>
2016-09-28 08:56:34 +0200

    * remove explicit compile call, its no longer necessary because we don't need the tag instances any longer

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,96 +1,94 @@
 const exec = require('child_process').execFile;
+const bus = riot.observable();
 
-var bus = riot.observable();
-riot.compile(function() {
-  riot.mount('watchversion', { bus: bus });
-  riot.mount('updatebutton', { bus: bus });
-  riot.mount('icon', { bus: bus });
-  riot.mount('filelist', { bus: bus });
-  riot.mount('successdialog', { bus: bus });
+riot.mount('watchversion', { bus: bus });
+riot.mount('updatebutton', { bus: bus });
+riot.mount('icon', { bus: bus });
+riot.mount('filelist', { bus: bus });
+riot.mount('successdialog', { bus: bus });
 
-  function getVersion() {
-    bus.trigger('watch.progress.start');
-    exec('/usr/local/bin/ttwatch', ['-v'],
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.error(error);
-          bus.trigger('watch.version.error', 'It seems your watch is not connected.');
-        } else {
-          bus.trigger('watch.version.success', stdout);
-        }
-          bus.trigger('watch.progress.stop');
+function getVersion() {
+  bus.trigger('watch.progress.start');
+  exec('/usr/local/bin/ttwatch', ['-v'],
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.error(error);
+        bus.trigger('watch.version.error', 'It seems your watch is not connected.');
+      } else {
+        bus.trigger('watch.version.success', stdout);
       }
-    );
-  }
-
-  bus.on('watch.update.firmware', function() {
-    bus.trigger('watch.progress.start');
-    exec('/usr/local/bin/ttwatch', ['--update-fw'],
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.error(error);
-         bus.trigger('watch.successdialog.message', stderr);
-        } else {
-          let message = stdout;
-          if (message === '') {
-            // Thats a fallback because in case of up to date the output goes to stderr but the app is returning with successs
-            message = stderr;
-          }
-          bus.trigger('watch.successdialog.message', message);
-        }
         bus.trigger('watch.progress.stop');
-      }
-    );
-  });
+    }
+  );
+}
 
-  bus.on('watch.update.gps', function() {
-    bus.trigger('watch.progress.start');
-    exec('/usr/local/bin/ttwatch', ['--update-gps'],
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.error(error);
-         bus.trigger('watch.successdialog.message', stderr);
-        } else {
-          bus.trigger('watch.successdialog.message', stdout);
+getVersion();
+setInterval(getVersion, 5000);
+
+bus.on('watch.update.firmware', function() {
+  bus.trigger('watch.progress.start');
+  exec('/usr/local/bin/ttwatch', ['--update-fw'],
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.error(error);
+       bus.trigger('watch.successdialog.message', stderr);
+      } else {
+        let message = stdout;
+        if (message === '') {
+          // Thats a fallback because in case of up to date the output goes to stderr but the app is returning with successs
+          message = stderr;
         }
-       bus.trigger('watch.progress.stop');
+        bus.trigger('watch.successdialog.message', message);
       }
-    );
+      bus.trigger('watch.progress.stop');
+    }
+  );
   });
 
-  bus.on('watch.update.time', function() {
-    bus.trigger('watch.progress.start');
-    exec('/usr/local/bin/ttwatch', ['--set-time'],
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.error(error);
-          bus.trigger('watch.successdialog.message', stderr);
-        } else {
-          bus.trigger('watch.successdialog.message', 'Successfully updated the time on the watch.');
-        }
-        bus.trigger('watch.progress.stop');
+bus.on('watch.update.gps', function() {
+  bus.trigger('watch.progress.start');
+  exec('/usr/local/bin/ttwatch', ['--update-gps'],
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.error(error);
+        bus.trigger('watch.successdialog.message', stderr);
+      } else {
+        bus.trigger('watch.successdialog.message', stdout);
       }
-    );
-  });
+     bus.trigger('watch.progress.stop');
+    }
+  );
+});
 
-  bus.on('watch.download.activities', function() {
-    bus.trigger('watch.progress.start');
-    exec('/usr/local/bin/ttwatch', ['--activity-store=/home/ms/.ttws', '--get-activities'],
-      (error, stdout, stderr) => {
-        if (error !== null) {
-          console.error(error);
-          bus.trigger('watch.successdialog.message', stderr);
-        } else {
-          bus.trigger('watch.successdialog.message', 'Successfully downloaded the latest activities from the watch.');
-          bus.trigger('watch.activities.update');
-        }
-        bus.trigger('watch.progress.stop');
+bus.on('watch.update.time', function() {
+  bus.trigger('watch.progress.start');
+  exec('/usr/local/bin/ttwatch', ['--set-time'],
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.error(error);
+        bus.trigger('watch.successdialog.message', stderr);
+      } else {
+        bus.trigger('watch.successdialog.message', 'Successfully updated the time on the watch.');
       }
-    );
-  });
+      bus.trigger('watch.progress.stop');
+    }
+  );
+});
 
-  getVersion();
-  setInterval(getVersion, 5000);
+bus.on('watch.download.activities', function() {
+  bus.trigger('watch.progress.start');
+  exec('/usr/local/bin/ttwatch', ['--activity-store=/home/ms/.ttws', '--get-activities'],
+    (error, stdout, stderr) => {
+      if (error !== null) {
+        console.error(error);
+        bus.trigger('watch.successdialog.message', stderr);
+      } else {
+        bus.trigger('watch.successdialog.message', 'Successfully downloaded the latest activities from the watch.');
+        bus.trigger('watch.activities.update');
+      }
+      bus.trigger('watch.progress.stop');
+    }
+  );
 });
 
 bus.on('local.activities.convert', function(item) {
